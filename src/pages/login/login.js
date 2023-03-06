00b68a7bb3d097d3b0d9a0544d723fceba9d9618
Tareq El-Ali <elalitareq@gmail.com>
2023-03-06 12:46:32 +0200

    addid some stuff

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,21 @@
-import "./login.css"
-
+import "./login.css";
+function handleLogin(event){
+    event.preventDefault();
+}
 function Login(props) {
-    return ( 
-        <h1>
-            this is the login page
-        </h1>
-     );
+  return <>
+  
+  <form onSubmit={handleLogin}>
+    <h1 >
+
+    </h1>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email"/>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password"/>
+    <button type="submit" name="login" id="login">Login</button>
+  </form>
+  </>;
 }
 
 export default Login;
