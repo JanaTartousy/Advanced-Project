9f7657d8904b887a8b92187c55dc010c0731f84a
Tareq El-Ali <elalitareq@gmail.com>
2023-03-06 22:26:49 +0200

    adding use context for authentication

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ import Teams from "./pages/teams/teams";
 import Reports from "./pages/reports/reports";
 import Login from "./pages/login/login";
 import Home from "./pages/home/home";
+import { useState } from "react";
+import { UserContext } from "./userContext";
+import { Cookie } from "universal-cookie";
+const cookie = new Cookie()
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  setToken(cookie.get('auth-token'))
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/employees" element={<Employees />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/reports" element={<Reports />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
+        <UserContext.Provider value={{ token, isLoggedIn ,setToken,setIsLoggedIn}}>
+          <Route path="/" element={<Home />}>
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/reports" element={<Reports />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </UserContext.Provider>
       </Routes>
     </div>
   );
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import "./home.css";
+
 function Home() {
+
   return (
     <>
       <h1>home</h1>
-      <Outlet/>
+      <Outlet />
     </>
   );
 }
