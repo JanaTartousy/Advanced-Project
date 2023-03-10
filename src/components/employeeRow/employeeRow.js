69e71d92a64bc449559b1a70f2b3086690ba58b7
JanaTartousy <tartousyjana@gmail.com>
2023-03-10 08:56:45 +0200

    employee buttons

--- src/components/employeeRow/employeeRow.js
+++ src/components/employeeRow/employeeRow.js
@@ -6,22 +6,29 @@ import Delete from "@mui/icons-material/Delete";
 const columns = [
   { field: "id", headerName: "ID", width: 90 },
   {
-    field: "employeeName",
-    headerName: "Employee name",
-    width: 610,
-    editable: true,
+    field: "firstName",
+    headerName: "First name",
+    width: 250,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 800,
   },
   {
     field: "edit",
     headerName: "Edit",
-    width: 150,
+    width: 100,
     renderCell: () => (
       <Edit
         name="edit employee"
         variant="contained"
         sx={{
           color: "#333",
-          "&:hover": {},
+          "&:hover": {
+            transform: "scale(1.3)",
+            transition: "0.2s ease-out",
+          },
         }}
       ></Edit>
     ),
@@ -29,14 +36,17 @@ const columns = [
   {
     field: "delete",
     headerName: "Delete",
-    width: 200,
+    width: 100,
     renderCell: () => (
       <Delete
         name="delete employee"
         variant="contained"
         sx={{
           color: "rgb(219 28 28)",
-          "&:hover": {},
+          "&:hover": {
+            transform: "scale(1.3)",
+            transition: "0.2s ease-out",
+          },
         }}
       ></Delete>
     ),
@@ -44,15 +54,15 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, employeeName: "Jon" },
-  { id: 2, employeeName: "Cersei" },
-  { id: 3, employeeName: "Jaime" },
-  { id: 4, employeeName: "Arya" },
-  { id: 5, employeeName: "Daenerys" },
-  { id: 6, employeeName: "Bella" },
-  { id: 7, employeeName: "Ferrara" },
-  { id: 8, employeeName: "Rossini" },
-  { id: 9, employeeName: "Harvey" },
+  { id: 1, firstName: "Jon", lastName: "Snow" },
+  { id: 2, firstName: "Cersei", lastName: "Lannister" },
+  { id: 3, firstName: "Jaime", lastName: "Lannister" },
+  { id: 4, firstName: "Arya", lastName: "Stark" },
+  { id: 5, firstName: "Daenerys", lastName: "Targaryen" },
+  { id: 6, firstName: "Bella", lastName: "Melisandre" },
+  { id: 7, firstName: "Ferrara", lastName: "Clifford" },
+  { id: 8, firstName: "Rossini", lastName: "Frances" },
+  { id: 9, firstName: "Harvey", lastName: "Roxie" },
 ];
 
 export default function DataGridDemo() {
@@ -70,7 +80,7 @@ export default function DataGridDemo() {
       pageSizeOptions={[10]}
       disableRowSelectionOnClick
       sx={{
-        backgroundColor: '#f4f9fc',
+        backgroundColor: "#f4f9fc",
       }}
     />
   );
--- src/pages/employees/employees.js
+++ src/pages/employees/employees.js
@@ -3,42 +3,46 @@ import React from "react";
 import { Button } from "@mui/material";
 import EmployeeRow from "./../../components/employeeRow/employeeRow";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 const theme = createTheme({
   palette: {
-      white: { main: "#F6F8FA" },
-      accent: {main: "#369fff"},
-      accentderv: {main: "#69B7FF"},
-      gray: {main: "#707070"},
-      black: {main: "#333"},
+    white: { main: "#F6F8FA" },
+    accent: { main: "#369fff" },
+    accentderv: { main: "#69B7FF" },
+    gray: { main: "#707070" },
+    black: { main: "#333" },
   },
 });
 
 function Employees() {
   return (
     <div className="container">
-       <Button 
-        startIcon={<AddCircleIcon/>}
-         style={{
-            fontSize: 12,
-            backgroundColor: "#369fff",
-            color: "white",
-             left: 846, bottom: 12,
-         }}
-         
-         size="large"
-         onClick={()=>alert('You can now add an employee')}
-         variant="contained"
-        >
-            Add
-        </Button>
+      <Button
+        startIcon={<AddCircleIcon />}
+        style={{
+          fontSize: 12,
+          backgroundColor: "#369fff",
+          color: "white",
+          left: 1250,
+          bottom: 12,
+        }}
+        size="large"
+        variant="contained"
+        sx={{
+          "&:hover": {
+            transform: "scale(1.05)",
+            transition: "0.2s ease-out",
+          },
+        }}
+      >
+        Add
+      </Button>
       <div className="employee-header">
         <h2>Employees</h2>
-        <div style={{ position: 'relative' }}/>
-       
+        <div style={{ position: "relative" }} />
       </div>
-      <EmployeeRow/>
+      <EmployeeRow />
     </div>
   );
 }
