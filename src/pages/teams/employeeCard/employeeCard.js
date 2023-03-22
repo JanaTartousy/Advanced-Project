530e95d260b6525f4971390fbe0ac19d9cb4aba2
Tareq El-Ali <elalitareq@gmail.com>
2023-03-22 12:54:41 +0200

    fixed navigation onclick

--- src/pages/teams/employeeCard/employeeCard.js
+++ src/pages/teams/employeeCard/employeeCard.js
@@ -1,10 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import "./employeeCard.css"
 
 function EmployeeCard(props) {
     const { employee } = props;
-  
+    const navigate=useNavigate()
     return (
-      <div className="employee-card">
+      <div className="employee-card" onClick={()=>{navigate(`/profile/${employee.id}`)}}>
         <img src={employee.picture} alt="Profile" />
           <h3>{employee.first_name} {employee.last_name}</h3>
       </div>
